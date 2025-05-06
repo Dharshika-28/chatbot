51672408a067f67ecda28ea3b18c07479ed9d068
Dharshika-28 <dharshikar2k2@gmail.com>
2025-05-07 00:04:33 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { WelcomeScreen } from './components/welcome-screen'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <WelcomeScreen />
+      </div>
+    </>
+  )
+}
+
+export default App
